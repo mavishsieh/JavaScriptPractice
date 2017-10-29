c2233d40347a5849d55bb01ebaa063dd53988562
Hsieh Mu Hua <mavis@Hsiehs-MacBook-Pro.local>
2017-10-29 19:53:38 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,151 @@
+// Lecture: variable
+/*var name = 'John';
+console.log(name)
+
+var lastName = 'Smith';
+console.log(lastName)
+
+var age = 26;
+console.log(age)
+
+var fullAge = true
+console.log(fullAge)
+*/
+
+// Lecture: variables 2
+/*
+var name = 'John'
+var age = 26
+console.log(name + age)
+
+var job, isMarried;
+console.log(job)
+
+job = 'developer';
+isMarried = false;
+
+console.log(name + ' ' + job + ' ' + age + ' ' + isMarried)
+
+var lastName = prompt('What is the last name?')
+console.log('lastName is ' + lastName);
+
+alert('this is Alear!!')
+*/
+
+// Lecture 3: operators
+//var name = 'John';
+//var age = 26;
+//var isMarried = 'no';
+//
+//if(isMarried === 'yes') {
+//    console.log(name + ' is married');
+//} else {
+//    console.log(name + ' will hopefully marry soon :)');
+//}
+//
+//
+//var job = prompt('What does John do?');
+//
+//switch (job) {
+//    case 'teacher':
+//        console.log('John teaches kids.');
+//        break;
+//    case 'driver':
+//        console.log('John drives a cab in Lisbon.');
+//        break;
+//    case 'cop':
+//        console.log('John helps fight crime.');
+//        break;
+//    default:
+//        console.log('Jone does something else.');
+//}
+//
+
+//var height1 = 160
+//var age1 = 26
+//
+//var height2 = 163
+//var age2 = 23
+//
+//var height3 = 158
+//var age3 = 30
+//
+//var score1 = height1 + age1 * 5
+//var score2 = height2 + age2 * 5
+//var score3 = height3 + age3 * 5
+//
+//if(score1 > score2 && score1 > score3) {
+//    console.log('player1(' + score1 +') wins.');
+//} else if(score2 > score1 && score2 > score3) {
+//    console.log('player2(' + score2 +') wins.');
+//} else if (score3 > score1 && score3 > score2) {
+//    console.log('player3(' + score3 +') wins.');
+//} else {
+//    console.log('It\'s a draw');
+//}
+
+
+//function calculateAge(yearOfBirth) {
+//    var age = 2016 - yearOfBirth;
+//    return age
+//}
+//
+//console.log(calculateAge(1940))
+//
+//yearsUntilRetirement('Mavis', 1950)
+//
+//function yearsUntilRetirement(name, yearOfBirth) {
+//    var years = 65 - calculateAge(yearOfBirth)
+//    
+//    if(years > 0) {
+//        console.log(name + ' retires in ' + years + ' years.');
+//    } else {
+//        console.log(name + ' is already retired.')
+//    }
+//}
+
+
+// function statement
+function someFun(par) {
+    // code
+}
+
+// function expression
+var someFun = function(par) {
+    // code
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
